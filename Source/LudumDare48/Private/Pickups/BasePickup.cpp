e66cd080fa0b6e73a527447180a8ea443db33920
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-04-24 22:41:57 +0300

    Fix BasePickup

--- Source/LudumDare48/Private/Pickups/BasePickup.cpp
+++ Source/LudumDare48/Private/Pickups/BasePickup.cpp
@@ -1,7 +1,7 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "BasePickup.h"
+#include "Pickups/BasePickup.h"
 #include "Characters/PlayerCharacter.h"
 
 
