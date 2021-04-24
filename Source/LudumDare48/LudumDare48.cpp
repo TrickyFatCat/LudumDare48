9230d8762dcf36f2e75aba7b7143596ba7e9864b
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-04-24 12:14:24 +0300

    Create C++ solution

---
+++ Source/LudumDare48/LudumDare48.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LudumDare48.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LudumDare48, "LudumDare48" );
