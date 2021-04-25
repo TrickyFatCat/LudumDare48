44e7f77ee1bde4e2c137dcb5f412d1a0afd7cde3
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-04-25 07:51:14 +0300

    Create Interaction interface

---
+++ Source/LudumDare48/Private/Interfaces/Interaction.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/Interaction.h"
+
+
+// Add default functionality here for any IInteraction functions that are not pure virtual.
