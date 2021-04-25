bc04a04a6d2779a2a266f2d50631f3c3ba188dc4
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-04-25 20:47:32 +0300

    Implemen win sequence

--- Source/LudumDare48/Public/Characters/PlayerCharacter.h
+++ Source/LudumDare48/Public/Characters/PlayerCharacter.h
@@ -41,6 +41,8 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMagicIncreased, int32, Coins);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnKeyAdded, EKeyColor, KeyColor);
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnStartSpawn);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPlayerWin);
 /**
  * 
  */
@@ -66,6 +68,8 @@ public:
 
 	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category="Player Character|Spawn")
 	FOnStartSpawn OnPlayerStartSpawn;
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category="Player Character|Spawn")
+	FOnPlayerWin OnPlayerWin;
 
 	// Movement
 private:
